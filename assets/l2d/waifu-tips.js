13fcf436174d975853274c09c053b9fc57191333
BillZhou233 <billzhou233@yeah.net>
2019-08-10 13:42:11 +0800

    Update waifu-tips.js

--- assets/l2d/waifu-tips.js
+++ assets/l2d/waifu-tips.js
@@ -209,6 +209,7 @@ function loadTipsMessage(result) {
         $(document).on("mouseover", tips.selector, function (){
             var text = getRandText(tips.text);
             text = text.render({text: $(this).text()});
+            text = text.render({title: $(this).attr("title")});
             showMessage(text, 3000);
         });
     });
